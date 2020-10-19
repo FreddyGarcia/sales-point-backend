8e46f0ad4b9d8cee72477999c8da32775d535705
Freddy Garcia <freddie-wpy@outlook.es>
2020-10-19 00:13:23 -0400

    Add session authentication to API

--- SalesPoint/settings.py
+++ SalesPoint/settings.py
@@ -140,6 +140,8 @@ STATIC_ROOT = './assets'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
