c8815a1145ae1dc3697a88b2f5636f3da4ae4fb7
Freddy Garcia <freddie-wpy@outlook.es>
2020-10-25 23:53:03 -0400

    Add audit data on create/update

--- core/serializers.py
+++ core/serializers.py
@@ -4,8 +4,25 @@ from .models import *
 
 # Create your models here.
 class BaseSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        Model = self.context.get('view').get_serializer_class().Meta.model
+        user = self.context.get('request').user
+        validated_data['created_by'] = user
+        validated_data['updated_by'] = user
+        obj = Model.objects.create(**validated_data)
+        obj.save()
+        return obj
+
+    def update(self, instance, validated_data):
+        user = self.context.get('request').user
+        instance.__dict__.update(validated_data)
+        instance.updated_by = user
+        instance.save()
+        return instance
+
     class Meta:
-        exclude = ['created_at', 'updated_at']
+        exclude = ['created_at', 'updated_at', 'is_enabled', 'created_by', 'updated_by']
 
 
 class CompanySerializer(BaseSerializer):
@@ -15,16 +32,11 @@ class CompanySerializer(BaseSerializer):
 
 
 class BranchSerializer(BaseSerializer):
-    company = CompanySerializer()
+    company = CompanySerializer(read_only=True)
+    company_id = serializers.CharField()
 
     class Meta(BaseSerializer.Meta):
         model = Branch
-
-
-class company(BaseSerializer):
-
-    class Meta(BaseSerializer.Meta):
-        model = Company
 
 
 class ProductLineSerializer(BaseSerializer):
