bb96a6fb330da33df8e223975c52c0cb9f8bcbac
Freddy Garcia <freddie-wpy@outlook.es>
2020-10-29 22:17:55 -0400

    Update model data on save

--- core/admin.py
+++ core/admin.py
@@ -3,10 +3,18 @@ from .models import *
 # Register your models here.
 
 class BaseModelAdmin(admin.ModelAdmin):
-      readonly_fields=('created_by', 'updated_by')
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        obj.save()
+        super().save_model(request, obj, form, change)
+
+    readonly_fields=('created_by', 'updated_by')
 
 admin.site.register(Company, BaseModelAdmin)
 admin.site.register(Branch, BaseModelAdmin)
+admin.site.register(BranchAddress, BaseModelAdmin)
 admin.site.register(ProductFamily, BaseModelAdmin)
 admin.site.register(ProductLine, BaseModelAdmin)
 admin.site.register(ProductMeasureUnit, BaseModelAdmin)
