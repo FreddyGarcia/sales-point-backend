7df2a62a4beca396093ce6631b8b65208b69c9ef
Freddy Garcia <freddie-wpy@outlook.es>
2020-11-04 21:50:26 -0400

    Hotfix

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import api
-from . import viewsets
 
 router = routers.DefaultRouter()
 router.register('branch', api.BranchViewSet)
